cb2361c5903a12731f0769f3476a2e9dfef7aaf5
cpu1001 <cpu.cpu@hotmail.com>
2018-04-05 22:22:50 +0200

    auth and routes

---
+++ server/src/controllers/AuthenticationController.js
@@ -0,0 +1,9 @@
+const AuthenticationController = require('./controllers/AuthenticationController')
+
+module.exports = {
+    register (req, res) {
+        res.send({
+            message: 'Hello ${req.body.email}! Your user was registered. Have fun!'
+        })
+    }
+}
